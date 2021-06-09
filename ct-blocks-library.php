cbc647c80a1f2b27aba3c22ff7d766fcdc6967c1
Khoi Nguyen <khoi@codetot.com>
2021-06-09 21:11:11 +0700

    Add class to enqueue assets in editor

---
+++ ct-blocks-library.php
@@ -0,0 +1,27 @@
+<?php
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Plugin Name:       CT Blocks Library
+ * Plugin URI:        https://codetot.com
+ * Version:           1.0.0
+ * Author:            CODE TOT JSC
+ * Author URI:        https://codetot.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       ct-blocks-library
+ * Domain Path:       /languages
+ */
+
+define( 'CODETOT_BLOCKS_LIBRARY_VERSION', '1.0.0' );
+define( 'CODETOT_BLOCKS_LIBRARY_PLUGIN_SLUG', 'ct-blocks-library' );
+define( 'CODETOT_BLOCKS_LIBRARY_PLUGIN_NAME', esc_html_x('CT Blocks Library', 'plugin name', 'ct-blocks-library'));
+define( 'CODETOT_BLOCKS_LIBRARY_DIR', plugin_dir_path(__FILE__));
+define( 'CODETOT_BLOCKS_LIBRARY_AUTHOR', 'Code Tot JSC' );
+define( 'CODETOT_BLOCKS_LIBRARY_AUTHOR_URI', 'https://codetot.com');
+define( 'CODETOT_BLOCKS_LIBRARY_PLUGIN_URI', plugins_url('ct-blocks-library'));
+
+require_once CODETOT_BLOCKS_LIBRARY_DIR . '/includes/class-ct-blocks-library.php';
---
+++ includes/class-ct-blocks-library.php
@@ -0,0 +1,44 @@
+<?php
+
+class CT_Blocks_Library
+{
+  /**
+   * The single class instance.
+   *
+   * @var $instance
+   */
+  private static $instance;
+
+  public final static function instance()
+  {
+    if (is_null(self::$instance)) {
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
+
+  public function __construct()
+  {
+    $this->register_blocks();
+
+    add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ), 9 );
+  }
+
+  public function register_blocks() {
+
+  }
+
+  public function enqueue_block_editor_assets() {
+    $js_deps  = array( 'wp-block-editor', 'wp-blocks', 'wp-date', 'wp-i18n', 'wp-element', 'wp-edit-post', 'wp-compose', 'underscore', 'wp-hooks', 'wp-components', 'wp-keycodes', 'moment', 'jquery' );
+
+    wp_enqueue_script(
+      'ct-blocks-library-editor',
+      CODETOT_BLOCKS_LIBRARY_PLUGIN_URI . '/build/index.js',
+      filemtime( CODETOT_BLOCKS_LIBRARY_DIR . '/build/index.js' ),
+      $js_deps,
+      true
+    );
+  }
+}
+
+CT_Blocks_Library::instance();
